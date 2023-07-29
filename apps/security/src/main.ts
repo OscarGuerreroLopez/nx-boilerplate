19766db596a6503a5f5355bb4d5e4fdd0ed2ad1a
Oscar Guerrero <>
2023-07-29 09:53:16 +0200

    first commit

--- apps/security/src/main.ts
+++ apps/security/src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable security/detect-eval-with-expression */
 import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
@@ -6,7 +7,10 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  const userinput = req.body.userinput;
+  eval(userinput);
+
+  res.send({ message: 'Hello security API' });
 });
 
 app.listen(port, host, () => {
