7cca559152108f134c1b9eecb86207835ad9c7c1
Oscar Guerrero <>
2023-08-13 11:18:52 +0200

    Adding Error Handler test

---
+++ libs/common/src/lib/errorHandler.spec.ts
@@ -0,0 +1,40 @@
+import { ErrorHandler } from './errorHandler'; // Update with the correct import path
+import { logger, Severity } from './common';
+// Mock the logger module
+jest.mock('./common', () => {
+  const originalModule = jest.requireActual('./common');
+  return {
+    ...originalModule,
+    logger: {
+      info: jest.fn(),
+      warn: jest.fn(),
+      error: jest.fn(),
+    },
+  };
+});
+
+describe('ErrorHandler', () => {
+  it('should call the logger with the correct arguments', () => {
+    const error = new Error('Test Error');
+    const additionalErrorInfo = {
+      severity: Severity.ERROR,
+      service: 'test-service',
+      file: 'test-file',
+      property: 'test-property',
+      code: 'TEST_CODE',
+    };
+
+    ErrorHandler({ error, additionalErrorInfo });
+
+    // Replace this with the appropriate assertion based on your logger's structure
+    expect(logger.error).toHaveBeenCalledWith(
+      'Test Error',
+      expect.objectContaining({
+        service: 'test-service',
+        file: 'test-file',
+        property: 'test-property',
+        code: 'TEST_CODE',
+      })
+    );
+  });
+});
