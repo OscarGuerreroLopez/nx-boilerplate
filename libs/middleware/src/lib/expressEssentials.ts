4ddd689e5b2cb3dca7be881b7f7a89553230cac4
Oscar Guerrero <>
2023-08-19 16:42:06 +0200

    Adding checkExact

--- libs/middleware/src/lib/expressEssentials.ts
+++ libs/middleware/src/lib/expressEssentials.ts
@@ -3,6 +3,6 @@ import cors from 'cors';
 
 export const expressEssentials = (app: express.Express) => {
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+  app.use(express.json({ limit: '300kb' }));
   app.use(cors({ credentials: true, origin: true }));
 };
